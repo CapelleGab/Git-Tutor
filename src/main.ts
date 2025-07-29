de064e994986ce16bddf6f110b3d0abb25a7a7fe
CapelleGab <gabin.capelle.pro@gmail.com>
2025-07-29 15:26:32 +0200

    update main.ts

--- src/main.ts
+++ src/main.ts
@@ -6,29 +6,20 @@ import { checkPrerequisites } from './prerequisites.js'
 export async function main() {
   console.log('🧠 GitTutor - Checking environment...')
   await checkPrerequisites()
-
   console.log('')
-
-  console.log(
-    `
-.------------------------------------------------------------------------.
-|                                                                        |
-|      ____   ___   _             _____           _                      |
-|     / ___| |_ _| | |_          |_   _|  _   _  | |_    ___    _ __     |
-|    | |  _   | |  | __|  _____    | |   | | | | | __|  / _ \\  | '__|    |
-|    | |_| |  | |  | |_  |_____|   | |   | |_| | | |_  | (_) | | |       |
-|     \\____| |___|  \\__|           |_|    \\__,_|  \\__|  \\___/  |_|       |
-|                                                                        |
-|                                                                        |
-'------------------------------------------------------------------------'
-  `
-  )
-
+  console.log('.------------------------------------------------------------------------.')
+  console.log('|                                                                        |')
+  console.log('|      ____   ___   _             _____           _                      |')
+  console.log('|     / ___| |_ _| | |_          |_   _|  _   _  | |_    ___    _ __     |')
+  console.log("|    | |  _   | |  | __|  _____    | |   | | | | | __|  / _ \\  | '__|    |")
+  console.log('|    | |_| |  | |  | |_  |_____|   | |   | |_| | | |_  | (_) | | |       |')
+  console.log('|     \\____| |___|  \\__|           |_|    \\__,_|  \\__|  \\___/  |_|       |')
+  console.log('|                                                                        |')
+  console.log("'------------------------------------------------------------------------'")
+  console.log('')
   console.log('🎓 Welcome to GitTutor CLI ')
   console.log('You are about to learn Git in a fun and interactive way')
-
   console.log('')
-
   await menu()
 }
 
